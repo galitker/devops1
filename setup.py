76498054c8463d32b8d0d4dc9e4e530b19a808ad
Student <galit.cohen.kerner@gmail.com>
2017-07-06 20:46:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ setup(
     name='tryexcept',
     version='0.1',
     description='try_except',
-    url='http://github.com/galitker/try_except',
+    #url='http://github.com/galitker/devops1/tryexcept.py',
     author='Galit Kerner',
     author_email='galit.cohen.kerner@gmail.com',
     entry_points={
         'console_scripts': [
-            'try_except=home.student.tryexcept:main',
+            'tryexcept=home.student.tryexcept:main',
         ]
     },
     #license='Apache2',
-    packages=['tryexcept'],
+    packages=find_packages(),
     install_requires=requirements,
     zip_safe=False)
